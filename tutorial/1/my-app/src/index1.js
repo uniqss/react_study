b1072b8b4b2bb8a67c217694a5248cf2ac96d632
uniqss <uniqs@163.com>
2021-10-29 00:31:38 +0800

    3

--- tutorial/1/my-app/src/index1.js
+++ tutorial/1/my-app/src/index1.js
@@ -3,17 +3,10 @@ import ReactDOM from 'react-dom';
 import './index.css';
 
 class Square extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            value: null,
-        };
-    }
     render() {
         return (
-            // <button className="square" onClick={function () {console.log('click');}}>
-            <button className="square" onClick={ () => this.setState({value:'X'}) }>
-                {this.state.value}
+            <button className="square">
+                {/* TODO */}
             </button>
         );
     }
@@ -21,7 +14,7 @@ class Square extends React.Component {
 
 class Board extends React.Component {
     renderSquare(i) {
-        return <Square value={i}/>;
+        return <Square />;
     }
 
     render() {
