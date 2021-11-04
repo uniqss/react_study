ea03717fdd9d3c690d6ecbc1893727f67ffe513c
uniqss <uniqs@163.com>
2021-11-04 19:03:55 +0800

    must login

--- react_fullstack/client/src/pages/Home.js
+++ react_fullstack/client/src/pages/Home.js
@@ -2,38 +2,56 @@ import React, {useEffect, useState} from 'react'
 import axios from "axios";
 import {useHistory} from 'react-router-dom';
 import {SERVER_HOST} from '../HostCfg'
+import ThumbUpIcon from '@mui/icons-material/ThumbUp';
+import FavoriteIcon from '@mui/icons-material/Favorite';
+import ThumbUpAltIcon from '@mui/icons-material/ThumbUpAlt';
 
 export default function Home() {
 
     const [listOfPosts, setListOfPosts] = useState([]);
+    const [likedPosts, setLikedPosts] = useState([]);
     let history = useHistory();
 
     useEffect(() => {
-        axios.get(`${SERVER_HOST}/posts`).then((response) => {
+        axios.get(`${SERVER_HOST}/posts`, {headers: {accessToken: localStorage.getItem('accessToken')}}).then((response) => {
             // console.log(response.data);
-            setListOfPosts(response.data);
+            setListOfPosts(response.data.listOfPosts);
+            setLikedPosts(response.data.likedPosts.map((like) => {
+                return like.PostId;
+            }));
+            // console.log(response.data.likedPosts);
         });
     }, []);
 
     const likeAPost = (postId) => {
         axios.post(`${SERVER_HOST}/likes`,
             {PostId: postId},
-            {headers: {accessToken:localStorage.getItem('accessToken')}}
+            {headers: {accessToken: localStorage.getItem('accessToken')}}
         ).then((response) => {
             // alert(response.data);
             setListOfPosts(listOfPosts.map((post) => {
-                if (post.id === postId) {
-                    if (response.data.liked) {
-                        return {...post, Likes: [...post.Likes, 0]}
+                    if (post.id === postId) {
+                        if (response.data.liked) {
+                            return {...post, Likes: [...post.Likes, 0]}
+                        } else {
+                            const likesArray = post.Likes
+                            likesArray.pop();
+                            return {...post, Likes: likesArray}
+                        }
                     } else {
-                        const likesArray = post.Likes
-                        likesArray.pop();
-                        return {...post, Likes: likesArray}
+                        return post
                     }
-                } else {
-                    return post
-                }
-            }))
+                })
+            );
+
+            if (likedPosts.includes(postId)) {
+                setLikedPosts(likedPosts.filter((id) => {
+                        return id != postId
+                    })
+                );
+            } else {
+                setLikedPosts([...likedPosts, postId])
+            }
         });
     }
 
@@ -42,12 +60,17 @@ export default function Home() {
             {listOfPosts.map((value, key) => {
                 return <div className="post" key={key}>
                     <div className="title">{value.title}</div>
-                    <div className="body"  onClick={() => {
+                    <div className="body" onClick={() => {
                         history.push(`/post/${value.id}`)
                     }}>{value.postText}</div>
                     <div className="footer">
-                        {value.username}
-                        <button onClick={() => likeAPost(value.id)}>Like</button>
+                        <div className="username">{value.username}</div>
+                        <div className="buttons">
+                            <ThumbUpAltIcon
+                                onClick={() => likeAPost(value.id)}
+                                visibility={likedPosts.includes(value.id)}
+                            />
+                        </div>
                         <label>{value.Likes.length}</label>
                     </div>
                 </div>;
--- react_fullstack/server/routes/Posts.js
+++ react_fullstack/server/routes/Posts.js
@@ -2,10 +2,13 @@ const express = require('express');
 const router = express.Router();
 const {Posts, Likes} = require('../models');
 
+const {validateToken} = require('../middlewares/AuthMiddleware')
 
-router.get("/", async (req, res) => {
+router.get("/", validateToken, async (req, res) => {
     const listOfPosts = await Posts.findAll({include: [Likes]});
-    res.json(listOfPosts);
+
+    const likedPosts = await Likes.findAll({where: {UserId: req.user.id}})
+    res.json({listOfPosts:listOfPosts, likedPosts: likedPosts});
 });
 
 router.get("/byId/:id", async (req, res) => {
