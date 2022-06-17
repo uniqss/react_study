8dbbda57bfc23ae1b5cbff93566e79da6a13a124
uniqss <uniqs@163.com>
2022-06-17 16:03:52 +0800

    chart

--- ReactAdminDashboardTutorial/reactadmin/src/pages/home/Home.jsx
+++ ReactAdminDashboardTutorial/reactadmin/src/pages/home/Home.jsx
@@ -1,10 +1,14 @@
+import Chart from "../../components/chart/Chart"
 import FeaturedInfo from "../../components/featuredInfo/FeaturedInfo"
 import "./home.css"
+import { userData } from "../../dummyData"
 
 export default function Home() {
     return (
         <div className="home">
             <FeaturedInfo />
+            <Chart data={userData} title="User Analytics" grid dataKey="Active User" />
+            <div className="homeWidgets"></div>
         </div>
     )
 }
