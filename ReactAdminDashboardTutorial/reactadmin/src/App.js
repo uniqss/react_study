5d3c875b0535641979068d1f457e48266f64fd81
uniqss <uniqs@163.com>
2022-06-18 17:06:34 +0800

    new user

--- ReactAdminDashboardTutorial/reactadmin/src/App.js
+++ ReactAdminDashboardTutorial/reactadmin/src/App.js
@@ -10,6 +10,7 @@ import {
   // Link
 } from 'react-router-dom';
 import User from "./pages/user/User";
+import NewUser from "./pages/newUser/NewUser";
 
 
 function App() {
@@ -22,6 +23,7 @@ function App() {
           <Route path="/" element={<Home />} />
           <Route path="/users" element={<UserList />} />
           <Route path="/user/:userId" element={<User />} />
+          <Route path="/newUser" element={<NewUser />} />
         </Routes>
       </div>
     </BrowserRouter>
--- ReactAdminDashboardTutorial/reactadmin/src/components/sidebar/Sidebar.jsx
+++ ReactAdminDashboardTutorial/reactadmin/src/components/sidebar/Sidebar.jsx
@@ -14,7 +14,7 @@ import {
     WorkOutline,
     Report,
 } from "@material-ui/icons"
-// import { Link } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 export default function Sidebar() {
     return (
@@ -42,18 +42,18 @@ export default function Sidebar() {
                 <div className="sidebarMenu">
                     <h3 className="sidebarTitle">Quick Menu</h3>
                     <ul className="sidebarList">
-                        {/* <Link to="/users" className="link"> */}
-                        <li className="sidebarListItem">
-                            <PermIdentity className="sidebarIcon" />
-                            Users
-                        </li>
-                        {/* </Link> */}
-                        {/* <Link to="/products" className="link"> */}
-                        <li className="sidebarListItem">
-                            <Storefront className="sidebarIcon" />
-                            Products
-                        </li>
-                        {/* </Link> */}
+                        <Link to="/users" className="link">
+                            <li className="sidebarListItem">
+                                <PermIdentity className="sidebarIcon" />
+                                Users
+                            </li>
+                        </Link>
+                        <Link to="/products" className="link">
+                            <li className="sidebarListItem">
+                                <Storefront className="sidebarIcon" />
+                                Products
+                            </li>
+                        </Link>
                         <li className="sidebarListItem">
                             <AttachMoney className="sidebarIcon" />
                             Transactions
